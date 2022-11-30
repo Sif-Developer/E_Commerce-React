d843556484089dc9eb4d013db310b1e37a668dc6
SergioJ-Vidal <113844469+SergioJ-Vidal@users.noreply.github.com>
2022-11-30 08:53:05 +0100

    Register Component Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+       <BrowserRouter>
+        <GlobalProvider>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </GlobalProvider>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
