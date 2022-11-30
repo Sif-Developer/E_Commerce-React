a3753d102a810f556daa56153c073e28e868ea47
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-11-30 12:24:40 +0100

    Header connected to app

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,1 +1,14 @@
-//New branch
+import React from "react";
+
+function Header(){
+    return(
+        <nav>
+            <div>
+                <h1>yee</h1>
+            </div>
+        </nav>
+    )
+}
+
+
+export default Header
