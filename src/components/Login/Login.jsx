4a2e5b5c4ea0755dbec09c913148ea25ecd38670
SergioJ-Vidal <113844469+SergioJ-Vidal@users.noreply.github.com>
2022-11-30 22:19:52 +0100

    Modal Login component OK

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,8 @@
-import React, { useContext, useEffect } from "react";
-import { Button, Form, Input } from "antd";
+import React, { useContext, useEffect, useState } from "react";
+import { Button, Form, Input, Modal } from "antd";
 import { UserContext } from "../../Context/UserContext/UserState";
 import "./Login.scss";
 import { useNavigate } from "react-router-dom";
-
 
 const Login = () => {
   const { login } = useContext(UserContext);
@@ -12,65 +11,88 @@ const Login = () => {
     login(values);
   };
 
-  // useEffect(() => {
-  //   const foundToken = JSON.parse(localStorage.getItem("token"));
-  //   if (foundToken) {
-  //     navigate("/profile")
-  //   }
-  // }, [login])
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const handleOk = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setOpen(false);
+    }, 3000);
+  };
+
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    const foundToken = JSON.parse(localStorage.getItem("token"));
+    if (foundToken) {
+      navigate("/profile")
+    }
+
+  }, [login])
+
+
   return (
-    <div className="container">
-      <Form className="form-container"
-        name="basic"
-        labelCol={{
-          span: 8,
-        }}
-        wrapperCol={{
-          span: 16,
-        }}
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-        autoComplete="off"
-      >
-        <Form.Item
-          label ="Email"
-          name="email"
-          rules={[
-            {
-              required: true,
-              type: "email",
-              message: "Please input your email!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
 
-        <Form.Item
-          label = "Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your password!",
-            },
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item
-          
-        >
-          <Button className="send-btn" type="primary" htmlType="submit">
+    <>
+      <Button type="primary" onClick={showModal}>
+        Login
+      </Button>
+      <Modal className="modal-form" open={open} title="Login to your user" onOk={handleOk} onCancel={handleCancel}
+        footer={[
+          <Button key="back" onClick={handleCancel}>
+            Return
+          </Button>,
+          <Button key="submit" type="primary" form="myform" value="Submit" htmlType="submit" loading={loading} onClick={handleOk}>
             Login
           </Button>
-        </Form.Item>
-      </Form>
-    </div>
+        ]}
+      >
+        <div className="container">
+          <Form className="form-container" id="myform" name="basic"
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 16,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            autoComplete="off"
+          >
+            <Form.Item label="Email" name="email"
+              rules={[
+                {
+                  required: true,
+                  type: "email",
+                  message: "Please input your email!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item label="Password" name="password"
+              rules={[{ required: true, message: "Please input your password!" }
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+          </Form>
+        </div>
+      </Modal>
+    </>
   );
+
 };
 
 
