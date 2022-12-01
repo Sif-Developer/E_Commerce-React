9efb9706b568b5079c7d2ecc3c23f85f366053a6
SergioJ-Vidal <113844469+SergioJ-Vidal@users.noreply.github.com>
2022-12-01 13:23:25 +0100

    Folder Product

---
+++ src/Context/ProductContext/ProductsState.js
@@ -0,0 +1,54 @@
+import React, { createContext, useReducer } from "react";
+import axios from "axios";
+import ProductsReducer from "./ProductsReducer";
+
+const initialState = {
+
+    products: [],
+
+};
+
+const API_URL = "http://localhost:3000";
+
+export const ProductsContext = createContext(initialState);
+
+export const ProductsProvider = ({ children }) => {
+
+    const [state, dispatch] = useReducer(ProductsReducer, initialState);
+
+    const getProducts = async () => {
+
+        const res = await axios.get(API_URL + "/products");
+
+        dispatch({
+
+            type: "GET_PRODUCTS",
+
+            payload: res.data,
+
+        });
+
+        return res;
+
+    };
+
+    return (
+
+        <ProductsContext.Provider
+
+            value={{
+
+                products: state.products,
+
+                getProducts
+
+            }}
+
+        >
+
+            {children}
+
+        </ProductsContext.Provider>
+
+    );
+}
