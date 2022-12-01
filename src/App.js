da6e7e50919b77ab2d2565109aa5b6a948d1e7d3
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-12-01 08:57:24 +0100

    merge sif

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Home from './components/Home/Home';
+import Register from './components/Register/Register';
 
 
 
