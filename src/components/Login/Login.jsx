1301719cdfb4b55b5d2577dc94c6135b67903ded
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-12-05 13:44:31 +0100

    registration modal has been created with the necessary fields. The data is collected in an array and the modal closes automatically when all fields are filled out. The code is fully commented to facilitate understanding

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -98,3 +98,7 @@ const Login = () => {
 
 
 export default Login;
+
+
+
+
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,79 +1,110 @@
 import React, { useState } from "react";
+import { Button, Modal, Form, Input } from "antd";
 
-export const RegisterForm = () => {
-  const [data, setData] = useState({
-    name: "",
-    email: "",
-  });
+const Register = () => {
+  // Estado para controlar si el modal se encuentra abierto o cerrado
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleInputChange=(event)=>{
-    console.log(event.target.name)
-    setData({
-      ...data,
-      [event.target.name]:event.target.value
-    
-    })
-  }
+  // Muestra el modal al hacer click en el botón "Register"
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
 
+  // Oculta el modal al hacer click en "OK"
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  // Oculta el modal al hacer click en "Cancel"
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  // Función que se ejecuta cuando se hace submit del formulario
+  const onFinish = (values) => {
+    console.log("Success:", values);
+    //Esto sirve para que cuando le acabe el formulario (si no hay errores) se cierre automáticamente
+    setIsModalOpen(false)
+  };
+
+  // Función que se ejecuta si falla el submit del formulario
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
     <>
-      <div>Register Form</div>
-      <form>
-        <input
-          type="text"
-          placeholder="name"
-          onChange={handleInputChange}
-          name="name"
-        />
+      {/* Botón que muestra el modal al hacer click en él */}
+      <Button type="primary" onClick={showModal}>
+        Register
+      </Button>
 
-        <input
-          type="email"
-          placeholder="email"
-          onChange={handleInputChange}
-          name="email"
-        />
+      {/* Modal que contiene el formulario */}
+      <Modal
+        title="Modal Register"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off">
 
-        <button type="submit">Enviar</button>
-      </form>
+          {/* Campo de name */}
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[{ required: true, message: "Please type your name!" }]}>
+            <Input />
+          </Form.Item>
+
+          {/* Campo de email */}
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please type your email!" }]}>
+            <Input />
+          </Form.Item>
+
+          {/* Campo de password */}
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please type your password!" }]}>
+            <Input />
+          </Form.Item>
+
+          {/* Campo de phone */}
+          <Form.Item
+            label="Phone"
+            name="phone"
+            rules={[
+              { required: true, message: "Please type your phone number!" },
+            ]}>
+            <Input />
+          </Form.Item>
+
+          {/* Campo de adress */}
+          <Form.Item
+            label="Address"
+            name="address"
+            rules={[{ required: true, message: "Please type your adress!" }]}>
+            <Input />
+          </Form.Item>
+
+          {/* Botón de submit */}
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </>
   );
 };
 
-export default RegisterForm
-
-{
-  /* <input
-type="text"
-placeholder="name"
-onChange={handleInputChange}
-name="name"
-/>
-
-<input
-type="email"
-placeholder="email"
-onChange={handleInputChange}
-name="email"
-/>
-
-<input
-type="text"
-placeholder="address"
-onChange={handleInputChange}
-name="address"
-/>
-
-<input
-type="password"
-placeholder="password"
-onChange={handleInputChange}
-name="password"
-/>
-
-<input
-type="file"
-placeholder="image"
-onChange={handleInputChange}
-name="image"
-/> */
-}
+export default Register;
