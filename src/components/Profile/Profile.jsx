28688cb4693c7dbc1aac22d3dbf07b909208a608
SergioJ-Vidal <113844469+SergioJ-Vidal@users.noreply.github.com>
2022-12-11 17:41:44 +0100

    Profile painted basic

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -15,12 +15,17 @@ const userLS = JSON.parse(localStorage.getItem("user"));
     getUser();
   }, []);
 
+  const imageUrl = "http://localhost:3000/images/users/" + userLS.image
+
   return (
     <div>
-      <h2>Profile</h2>
-      <p>{userLS.name}</p>
-      <p>{userLS.email}</p>
-      <p></p>
+      <h2>Your Profile</h2>
+      <p>Name: {userLS.name}</p>
+      <p>Email: {userLS.email}</p>
+      <img src={imageUrl}  className="userimg" alt="userimg"></img>
+      <span>Address: {userLS.address}</span>
+      <span>Phone: {userLS.phone}</span>
+      <span>Role: {userLS.role}</span>
     </div>
   );
 };
