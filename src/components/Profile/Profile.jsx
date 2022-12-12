03e45e0634248d5bbd4535b289e38cf2a577b9f9
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-12-12 20:25:09 +0100

    added a modal that shows the orders of the user logged

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,37 +1,64 @@
 import "./Profile.scss";
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import { useContext } from "react";
 import { UserContext } from "../../Context/UserContext/UserState";
+import { Button, Modal } from "antd";
 
 const Profile = () => {
   const { user, getUserLogged, OrdersNamed } = useContext(UserContext);
 
-  
-  
-  
-  
-  
+  const YourOrdersButton = () => {
+    const [visible, setVisible] = useState(false);
+
+    const showModal = () => {
+      setVisible(true);
+    };
+
+    const handleOk = () => {
+      setVisible(false);
+    };
+
+    const handleCancel = () => {
+      setVisible(false);
+    };
+
+    return (
+      <>
+        <Button type="primary" onClick={showModal}>
+          Your Orders
+        </Button>
+        <Modal
+          title="Your Orders"
+          visible={visible}
+          onOk={handleOk}
+          onCancel={handleCancel}>
+          {user.Orders?.map((order) =>
+            order.Products?.map((product) => <p>{product.name}</p>)
+          )}
+        </Modal>
+      </>
+    );
+  };
+
   useEffect(() => {
     getUserLogged();
   }, []);
-  
+
   const imageUrl = "http://localhost:3000/images/users/" + user.image;
-  
-  
+
   return (
     <div>
       <h2>Your Profile</h2>
       <p>Name: {user.name}</p>
       <p>Email: {user.email}</p>
       <img src={imageUrl} className="userimg" alt="userimg"></img>
-      <span>Address: {user.address}</span>
-      <span>Phone: {user.phone}</span>
-      <span>Role: {user.role}</span>
-      <span>
-        Orders :
-          {user.Orders?.map((order) => order.Products?.map(product => <p> Products name  {product.name}</p>))}
-      </span>
+      <div>Address: {user.address}</div>
+      <div>Phone: {user.phone}</div>
+      <spa>Role: {user.role}</spa>
+      <div>
+        <YourOrdersButton />
+      </div>
     </div>
   );
 };
